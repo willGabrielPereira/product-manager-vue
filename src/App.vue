1223e627d71e0a634639be4ad1d396bfad7ec668
William Gabriel Pereira <will.gabriel.pereira@gmail.com>
2023-05-25 02:01:57 +0000

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <div class="bg-blue-500">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
